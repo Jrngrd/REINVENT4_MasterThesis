75b77356b9c0837733a82f2deca1497e9a484742
aalexpersson <alex.persson0408@gmail.com>
2026-02-11 14:57:22 +0100

    split tack into correct train and held_out + func decomp

--- notebooks/prep_data.py
+++ notebooks/prep_data.py
@@ -7,35 +7,45 @@ def ensure_directory_exists(path):
     if not os.path.isdir(path):
         os.makedirs(path)
 
+def split_dataset(dataset, split):
+    """Splits a Hugging Face dataset into training and validation (hold out) sets."""
+    if split == "train":
+        return dataset[dataset["SMILES_Held_Out"] == False]["SMILES"]
+    else:
+        return dataset[dataset["SMILES_Held_Out"] == True]["SMILES"]
+
+def standardize_smiles(dataset):
+    """Standardizes SMILES strings in a dataset using RDKit and removes any that cannot be standardized."""
+    standardizer = RDKitStandardizer(filter_configs=None, isomeric=False)
+    ds = dataset.apply(standardizer.apply_filter)
+    return ds.dropna()
+
+def filter_and_save(dataset, allowed_tokens, base_path, filename):
+    """Filters out SMILES strings that contain tokens not in the allowed set, and saves the resulting dataset to a file."""
+    dataset = dataset[dataset.apply(lambda x: all(token in allowed_tokens for token in x))]
+    dataset.to_csv(f"{base_path}/{filename}", sep="\t", index=False, header=False)
+    
+
 def process_tack_data(base_path, allowed_tokens):
     """Downloads, filters, and splits the TACK dataset."""
     print("Downloading tack data")
     tack_ds = load_dataset("ailab-bio/TACK")
-    tack_ds_train = tack_ds["train"].to_pandas()
+    tack_ds = tack_ds["train"].to_pandas()
+
+    tack_smiles_train = split_dataset(tack_ds, "train")
+    print(f"Before standardization, TACK training dataset has {len(tack_smiles_train)} molecules")
+    tack_smiles_val = split_dataset(tack_ds, "val")
     
-    tack_smiles = tack_ds_train["SMILES"]
-
-    #FIXME Temporary: Replace all @ with $ to avoid tokenization issues with the model
-
     # RDKit standarization
-    standardizer = RDKitStandardizer(filter_configs=None, isomeric=False)
-    tack_smiles = tack_smiles.apply(standardizer.apply_filter)
+    tack_smiles_train = standardize_smiles(tack_smiles_train)
+    tack_smiles_val = standardize_smiles(tack_smiles_val)
 
-    # Remove all None values that may have been introduced by the standardization step
-    tack_smiles = tack_smiles.dropna()
-
-    print(f"After standardization, TACK dataset has {len(tack_smiles)} molecules", tack_smiles)
-    # Filter out SMILES that contain tokens not supported by the model
-    tack_smiles = tack_smiles[tack_smiles.apply(lambda x: all(token in allowed_tokens for token in x))]
-
-    n_head = int(0.8 * len(tack_smiles))
-    n_tail = len(tack_smiles) - n_head
-    print(f"number of molecules for: training={n_head}, validation={n_tail}")
-
-    train, validation = tack_smiles.head(n_head), tack_smiles.tail(n_tail)
+    print(f"After standardization, TACK training dataset has {len(tack_smiles_train)} molecules")
+    print(f"After standardization, TACK held-out dataset has {len(tack_smiles_val)} molecules")
     
-    train.to_csv(f"{base_path}/tack_train.smi", sep="\t", index=False, header=False)
-    validation.to_csv(f"{base_path}/tack_validation.smi", sep="\t", index=False, header=False)
+    # Filter out SMILES that contain tokens not supported by the model
+    filter_and_save(tack_smiles_train, allowed_tokens, base_path, "tack_train.smi")
+    filter_and_save(tack_smiles_val, allowed_tokens, base_path, "tack_validation.smi")
     print(f"Finished writing curated data to folder {base_path}")
 
 def process_synthetic_data(base_path, allowed_tokens):
@@ -47,8 +57,6 @@ def process_synthetic_data(base_path, allowed_tokens):
         # Remove labels and filter by tokens
         ds_split = synthetic_ds[split].remove_columns("labels")
 
-        #FIXME Temporary: Replace all @ with $ to avoid tokenization issues with the model
-
         # standardize SMILES and filter out those that contain tokens not supported by the model
         standardizer = RDKitStandardizer(filter_configs=None, isomeric=False)
         ds_split = ds_split.map(lambda x: {"text": standardizer.apply_filter(x["text"])})
@@ -56,7 +64,7 @@ def process_synthetic_data(base_path, allowed_tokens):
         # remove all None values that may have been introduced by the standardization step
         ds_split = ds_split.filter(lambda x: x["text"] is not None)
         
-
+        # Filter out SMILES that contain tokens not supported by the model
         ds_split = ds_split.filter(lambda x: all(token in allowed_tokens for token in x["text"]))
         
         print(f"After filtering, {split} split has {len(ds_split)} molecules")
